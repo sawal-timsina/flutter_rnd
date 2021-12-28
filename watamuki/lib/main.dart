cdf9015b20e69331023080782db0f4db92889a58
Sawal Timsina <t.sawal231@gmail.com>
2021-12-28 23:00:24 +0545

    feat: onboarding page

--- watamuki/lib/main.dart
+++ watamuki/lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 import 'package:sizer/sizer.dart';
 
 import 'src/config/routes/app_routes.dart';
@@ -9,11 +10,20 @@ import 'src/config/themes/app_theme.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
 
-  runApp(const Application());
+  SharedPreferences sharedPreferences = await SharedPreferences.getInstance();
+  final bool shouldShowOnboardingPage =
+      sharedPreferences.getBool('shouldShowOnboardingPage') ?? true;
+
+  runApp(Application(
+    shouldShowOnboardingPage: shouldShowOnboardingPage,
+  ));
 }
 
 class Application extends StatelessWidget {
-  const Application({Key? key}) : super(key: key);
+  final bool shouldShowOnboardingPage;
+
+  const Application({Key? key, required this.shouldShowOnboardingPage})
+      : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -25,6 +35,7 @@ class Application extends StatelessWidget {
         debugShowCheckedModeBanner: kDebugMode,
         title: "Watamuki",
         theme: AppTheme.light,
+        initialRoute: shouldShowOnboardingPage ? "/onboarding" : "/",
         onGenerateRoute: AppRoutes.onGenerateRoutes,
       );
     });
---
+++ watamuki/lib/src/presentation/pages/onboarding.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Onboarding extends StatelessWidget {
+  Onboarding({Key? key}) : super(key: key);
+
+  final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      top: false,
+      child: Scaffold(
+        body: ConstrainedBox(
+          constraints: const BoxConstraints.expand(),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              const Text(
+                'Onboarding',
+              ),
+              ElevatedButton(
+                  onPressed: () async {
+                    final SharedPreferences sharedPreferences = await _prefs;
+                    sharedPreferences
+                        .setBool('shouldShowOnboardingPage', false)
+                        .then((_) =>
+                            {Navigator.of(context).pushReplacementNamed("/")});
+                  },
+                  child: const Text("Get started"))
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
