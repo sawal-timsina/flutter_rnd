9d98f4faabd1f8f30899aa98a9cd08112b53b40c
Sawal Timsina <t.sawal231@gmail.com>
2022-01-07 00:31:48 +0545

    style: text field style added

--- watamuki/lib/src/config/themes/app_theme.dart
+++ watamuki/lib/src/config/themes/app_theme.dart
@@ -4,95 +4,135 @@ import 'package:google_fonts/google_fonts.dart';
 import 'colors.dart';
 
 class AppTheme {
+  static TextStyle headline1 = const TextStyle(
+    color: AppColors.black,
+    height: 48.0 / 36.0,
+    // line height calculated as is fontSize * height so, <expected-height>/<font-size>
+    fontSize: 36.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle headline2Bold = const TextStyle(
+    color: AppColors.black,
+    height: 36.0 / 18.0,
+    fontSize: 18.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle heading2Regular = const TextStyle(
+    color: AppColors.black,
+    height: 36.0 / 18.0,
+    fontSize: 18.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle button = const TextStyle(
+    color: AppColors.black,
+    height: 26.0 / 18.0,
+    fontSize: 18.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.w600,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle captionBold = const TextStyle(
+    color: AppColors.black,
+    height: 22.0 / 16.0,
+    fontSize: 16.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle captionRegular = const TextStyle(
+    color: AppColors.black,
+    height: 22.0 / 16.0,
+    fontSize: 16.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle textBold = const TextStyle(
+    color: AppColors.black,
+    height: 20.0 / 14.0,
+    fontSize: 14.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.bold,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle textRegular = const TextStyle(
+    color: AppColors.black,
+    height: 20.0 / 14.0,
+    fontSize: 14.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
+  static TextStyle subHeading = const TextStyle(
+    color: AppColors.black,
+    height: 20.0 / 12.0,
+    fontSize: 12.0,
+    fontStyle: FontStyle.normal,
+    fontWeight: FontWeight.normal,
+    leadingDistribution: TextLeadingDistribution.even,
+  );
+
   static ThemeData get light {
     return ThemeData(
+      brightness: Brightness.light,
       primarySwatch: AppColors.green,
       scaffoldBackgroundColor: Colors.white,
       visualDensity: VisualDensity.adaptivePlatformDensity,
       fontFamily: GoogleFonts.sourceSansPro().fontFamily,
-      textTheme: const TextTheme(
-        // heading-1
-        headline1: TextStyle(
-          color: AppColors.black,
-          height: 48.0 / 36.0,
-          // line height calculated as is fontSize * height so, <expected-height>/<font-size>
-          fontSize: 36.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.bold,
-          leadingDistribution: TextLeadingDistribution.even,
+      inputDecorationTheme: InputDecorationTheme(
+        labelStyle: captionBold,
+        floatingLabelStyle: captionBold,
+        hintStyle: captionRegular.copyWith(color: AppColors.greyDark),
+        counterStyle: subHeading.copyWith(
+          color: AppColors.greyDark,
         ),
-        // heading-2-bold
-        headline2: TextStyle(
-          color: AppColors.black,
-          height: 36.0 / 18.0,
-          fontSize: 18.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.bold,
-          leadingDistribution: TextLeadingDistribution.even,
+        errorStyle: subHeading.copyWith(color: Colors.red),
+        floatingLabelBehavior: FloatingLabelBehavior.always,
+        focusedErrorBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          borderSide: BorderSide(color: Colors.red, width: 1),
         ),
-        // heading-2-regular
-        headline3: TextStyle(
-          color: AppColors.black,
-          height: 36.0 / 18.0,
-          fontSize: 18.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.w600,
-          leadingDistribution: TextLeadingDistribution.even,
+        errorBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          borderSide: BorderSide(color: Colors.red, width: 1),
         ),
-        // button
-        button: TextStyle(
-          color: AppColors.black,
-          height: 26.0 / 18.0,
-          fontSize: 18.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.w600,
-          leadingDistribution: TextLeadingDistribution.even,
+        enabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          borderSide: BorderSide(color: AppColors.greyDark, width: 1),
         ),
-        // caption-bold
-        subtitle1: TextStyle(
-          color: AppColors.black,
-          height: 22.0 / 16.0,
-          fontSize: 16.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.bold,
-          leadingDistribution: TextLeadingDistribution.even,
+        disabledBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          borderSide: BorderSide(color: AppColors.greyDark, width: 1),
         ),
-        // caption-regular
-        subtitle2: TextStyle(
-          color: AppColors.black,
-          height: 22.0 / 16.0,
-          fontSize: 16.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.normal,
-          leadingDistribution: TextLeadingDistribution.even,
+        focusedBorder: const OutlineInputBorder(
+          borderRadius: BorderRadius.all(Radius.circular(12)),
+          borderSide: BorderSide(color: AppColors.green, width: 1),
         ),
-        // text-bold
-        bodyText1: TextStyle(
-          color: AppColors.black,
-          height: 20.0 / 14.0,
-          fontSize: 14.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.bold,
-          leadingDistribution: TextLeadingDistribution.even,
-        ),
-        // text-regular
-        bodyText2: TextStyle(
-          color: AppColors.black,
-          height: 20.0 / 14.0,
-          fontSize: 14.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.normal,
-          leadingDistribution: TextLeadingDistribution.even,
-        ),
-        // sub-heading
-        caption: TextStyle(
-          color: AppColors.black,
-          height: 20.0 / 12.0,
-          fontSize: 12.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.normal,
-          leadingDistribution: TextLeadingDistribution.even,
-        ),
+      ),
+      textTheme: TextTheme(
+        headline1: headline1,
+        headline2: headline2Bold,
+        headline3: heading2Regular,
+        subtitle1: captionBold,
+        subtitle2: captionRegular,
+        bodyText1: textBold,
+        bodyText2: textRegular,
+        caption: subHeading,
+        button: button,
       ),
       appBarTheme: const AppBarTheme(
         actionsIconTheme: IconThemeData(color: AppColors.black),
@@ -101,26 +141,12 @@ class AppTheme {
         iconTheme: IconThemeData(color: AppColors.black),
         titleTextStyle: TextStyle(color: AppColors.black),
       ),
-      bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+      bottomNavigationBarTheme: BottomNavigationBarThemeData(
         type: BottomNavigationBarType.fixed,
         elevation: 0,
         backgroundColor: Colors.white,
-        selectedLabelStyle: TextStyle(
-          color: AppColors.black,
-          height: 22.0 / 16.0,
-          fontSize: 16.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.bold,
-          leadingDistribution: TextLeadingDistribution.even,
-        ),
-        unselectedLabelStyle: TextStyle(
-          color: AppColors.black,
-          height: 22.0 / 16.0,
-          fontSize: 16.0,
-          fontStyle: FontStyle.normal,
-          fontWeight: FontWeight.normal,
-          leadingDistribution: TextLeadingDistribution.even,
-        ),
+        selectedLabelStyle: captionBold,
+        unselectedLabelStyle: captionRegular,
       ),
     );
   }
