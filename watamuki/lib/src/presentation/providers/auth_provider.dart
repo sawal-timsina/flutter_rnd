ca793415a3a2d53a5f5c2ba05e19c6abf4a69f72
Sawal Timsina <t.sawal231@gmail.com>
2022-01-14 23:05:30 +0545

    feat: auth user

--- watamuki/lib/src/presentation/providers/auth_provider.dart
+++ watamuki/lib/src/presentation/providers/auth_provider.dart
@@ -1,22 +1,40 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/foundation.dart' show ChangeNotifier;
+import 'package:watamuki/src/config/api/api.dart';
 import 'package:watamuki/src/config/firebase/auth.dart' show firebaseAuth;
+import 'package:watamuki/src/data/data_sources/remote/user_service.dart';
+import 'package:watamuki/src/data/models/user_modal.dart';
 
 class AuthProvider with ChangeNotifier {
+  late UserModal? _dbUser;
+
   AuthProvider() : super() {
     firebaseAuth.authStateChanges().listen((User? user) {
       if (user != null) {
         _user = user;
         _loggedIn = true;
+        getAuthUser();
       } else {
         _phoneVerified = false;
         _phoneNumber = "";
         _loggedIn = false;
         _user = null;
+        _dbUser = null;
       }
       notifyListeners();
     });
   }
+
+  void getAuthUser() {
+    kAPI.getService<UserService>().getAuthUser().then((res) {
+      _dbUser = res.body?.data;
+      _phoneVerified = (_dbUser?.isPhoneVerified)!;
+      _phoneNumber = (_dbUser?.phone)!;
+      notifyListeners();
+    });
+  }
+
+  UserModal? get dbUser => _dbUser;
 
   bool _loggedIn = false;
 
