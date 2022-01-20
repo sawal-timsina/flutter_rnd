a697697b7d4f2d2986b834f6e712b75ae4e6930e
Sawal Timsina <t.sawal231@gmail.com>
2022-01-20 18:32:15 +0545

    feat: user repository

---
+++ watamuki/lib/src/data/repositories/user/user_repository.dart
@@ -0,0 +1,6 @@
+import 'package:watamuki/src/core/resources/data_state.dart';
+import 'package:watamuki/src/data/models/user/user.dart';
+
+abstract class UserRepository {
+  Future<DataState<User>> getDbUser();
+}
