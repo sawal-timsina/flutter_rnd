b8592ef1723afc5baa65d46b2d1d25dc66b59445
Sawal Timsina <t.sawal231@gmail.com>
2022-01-21 21:59:11 +0545

    feat: facility params

--- lib/src/core/params/facility.dart
+++ lib/src/core/params/facility.dart
@@ -1,9 +1,19 @@
-class FacilityParams {
-  final int categoryId;
-  final String area;
+import 'package:json_annotation/json_annotation.dart';
+import 'package:watamuki/src/core/params/index.dart';
+
+part 'facility.g.dart';
+
+@JsonSerializable()
+class FacilityParams implements Params {
+  String? area;
+  @JsonKey(name: "category_id")
+  int? categoryId;
 
   FacilityParams({
-    this.categoryId = 1,
-    this.area = "",
+    this.categoryId,
+    this.area,
   });
+
+  @override
+  Map<String, dynamic> toJson() => _$FacilityParamsToJson(this);
 }
---
+++ lib/src/core/params/facility.g.dart
@@ -0,0 +1,13 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'facility.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+Map<String, dynamic> _$FacilityParamsToJson(FacilityParams instance) =>
+    <String, dynamic>{
+      'area': instance.area,
+      'category_id': instance.categoryId,
+    };
