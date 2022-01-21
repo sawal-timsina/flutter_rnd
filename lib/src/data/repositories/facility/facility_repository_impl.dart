f95d4b55723000cba5c5844746bab56c17456aed
Sawal Timsina <t.sawal231@gmail.com>
2022-01-21 22:01:16 +0545

    fix: naming convention

--- lib/src/data/repositories/facility/facility_repository_impl.dart
+++ lib/src/data/repositories/facility/facility_repository_impl.dart
@@ -6,16 +6,13 @@ import 'package:watamuki/src/data/data_sources/remote/facility/facility_service.
 import 'package:watamuki/src/data/models/facility/facility.dart';
 import 'package:watamuki/src/data/repositories/facility/facility_repository.dart';
 
-class FacilityRepositoryImpl implements FacilityRepository {
-  final FacilityService facilityService = kAPI.getService<FacilityService>();
+class FacilityRepositoryImpl with QueryProvider implements FacilityRepository {
+  final FacilityService _facilityService = kAPI.getService<FacilityService>();
 
   @override
   Future<DataState<List<Facility>>> getAllPublicFacility(params) async {
     try {
-      final response = await facilityService.getAllFacilities(
-        area: params.area,
-        categoryId: params.categoryId,
-      );
+      final response = await _facilityService.getAllFacilities(params.toJson());
 
       if (response.statusCode != HttpStatus.ok) {
         return DataError(response.bodyString);
