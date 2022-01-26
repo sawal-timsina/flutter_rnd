faa735305f241ab4f1a3ce46104b670a12821923
Sawal Timsina <t.sawal231@gmail.com>
2022-01-26 19:20:35 +0545

    feat: category service

---
+++ lib/src/services/category/category_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:watamuki/src/config/api/api.dart';
+import 'package:watamuki/src/core/utils/query_provider.dart';
+
+abstract class ICategoryService {
+  Future<Response> getAllCategory({QueryContext? context});
+}
+
+class CategoryService extends ICategoryService {
+  @override
+  Future<Response> getAllCategory({QueryContext? context}) {
+    final params = context?.queries[1];
+    return dio.get("/categories/public",
+        queryParameters: params != null ? params.toJson() : {});
+  }
+}
