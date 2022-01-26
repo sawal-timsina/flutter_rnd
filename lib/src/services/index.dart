82e4e0cdfbe3cf3852919e4cbc6f958d6bf8d1c9
Sawal Timsina <t.sawal231@gmail.com>
2022-01-26 19:21:00 +0545

    feat: services

---
+++ lib/src/services/index.dart
@@ -0,0 +1,7 @@
+import 'package:watamuki/src/services/category/category_service.dart';
+import 'package:watamuki/src/services/facility/facility_service.dart';
+import 'package:watamuki/src/services/user/user_service.dart';
+
+final userService = UserService();
+final categoryService = CategoryService();
+final facilityService = FacilityService();
