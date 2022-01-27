da2bcfee47be252a5c17abb461883bc343bfdfa6
Sawal Timsina <t.sawal231@gmail.com>
2022-01-27 19:01:07 +0545

    feat: coupon service

---
+++ lib/src/services/coupon/coupon_service.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:watamuki/src/config/api/api.dart';
+import 'package:watamuki/src/core/QueryProviders/models/modals.dart';
+
+abstract class ICouponService {
+  Future<Response> getAllCoupons({QueryContext? context});
+}
+
+class CouponService extends ICouponService {
+  @override
+  Future<Response> getAllCoupons({QueryContext? context}) {
+    final params = context?.pageParam[1];
+    return dio.get("/coupons/public",
+        queryParameters: params != null ? params.toJson() : {});
+  }
+}
