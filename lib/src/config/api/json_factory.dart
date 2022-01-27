7c364380e71af9c631f52bf334e21d3bae26f61b
Sawal Timsina <t.sawal231@gmail.com>
2022-01-27 19:02:03 +0545

    feat: json converter for coupon and user modal

--- lib/src/config/api/json_factory.dart
+++ lib/src/config/api/json_factory.dart
@@ -1,16 +1,22 @@
 import 'package:watamuki/src/core/QueryProviders/Converters/json_converter.dart';
 import 'package:watamuki/src/models/category/category.dart';
+import 'package:watamuki/src/models/coupon/index.dart';
 import 'package:watamuki/src/models/facility/facility.dart';
 import 'package:watamuki/src/models/user/user.dart';
 
 final jsonFactory = JsonConverter({
-  Category: (json) => Category.fromJson(json),
   User: (json) => User.fromJson(json),
+  Category: (json) => Category.fromJson(json),
+  Coupon: (json) => Coupon.fromJson(json),
+  Facility: (json) => Facility.fromJson(json),
 }, {
   List<Category>: (list) {
     return [for (final v in list) Category.fromJson(v)];
   },
   List<Facility>: (list) {
     return [for (final v in list) Facility.fromJson(v)];
+  },
+  List<Coupon>: (list) {
+    return [for (final v in list) Coupon.fromJson(v)];
   },
 });
