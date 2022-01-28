11a5b96e79845b63855397835ae6df0c3326bcdd
Sawal Timsina <t.sawal231@gmail.com>
2022-01-28 23:06:17 +0545

    feat: add == operator in indexable modal

--- lib/src/models/index.dart
+++ lib/src/models/index.dart
@@ -1,5 +1,17 @@
 import 'package:watamuki/src/core/QueryProviders/models/params.dart';
 
 abstract class Indexable extends Params {
+  late final int key;
+
+  Indexable(this.key);
+
   operator [](index) => toJson()[index];
+
+  @override
+  bool operator ==(Object other) {
+    return other is Indexable && other.key == key;
+  }
+
+  @override
+  int get hashCode => key.hashCode;
 }
