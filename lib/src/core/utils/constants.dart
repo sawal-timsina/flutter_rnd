350d8a3ba3683fc247695d7525aaf002c79e0f64
Sawal Timsina <t.sawal231@gmail.com>
2022-01-28 23:08:00 +0545

    feat: flatten list extension

--- lib/src/core/utils/constants.dart
+++ lib/src/core/utils/constants.dart
@@ -1,5 +1,9 @@
 import 'package:flutter/material.dart';
 
+class Constants {
+  static const pageSize = 15;
+}
+
 extension Merge on EdgeInsets {
   EdgeInsets merge(EdgeInsets? other) {
     return copyWith(
@@ -8,5 +12,34 @@ extension Merge on EdgeInsets {
       top: other?.top != 0 ? other?.top : top,
       bottom: other?.bottom != 0 ? other?.bottom : bottom,
     );
+  }
+}
+
+bool _isList(list) => list is List;
+
+bool _checkArray(List list) {
+  if (list.isEmpty) return true;
+
+  var flag = _isList(list[0]);
+  var length = flag ? list[0].length : 0;
+  for (var i = 0; i < list.length; i++) {
+    var tempFlag = _isList(list[i]);
+    var tempLength = tempFlag ? list[i].length : 0;
+    if (flag != tempFlag || length != tempLength) return false;
+  }
+  return true;
+}
+
+extension Flatten on List {
+  List flatten() {
+    final _shapeCheck = _checkArray(this);
+    if (!_shapeCheck) throw Exception('Uneven array dimension');
+    var result = [];
+    for (var i = 0; i < length; i++) {
+      for (var j = 0; j < this[i].length; j++) {
+        result.add(this[i][j]);
+      }
+    }
+    return result;
   }
 }
