d01203a7abeef1b4e121a71d4a1c0914f06eff3a
Sawal Timsina <t.sawal231@gmail.com>
2022-02-01 09:35:41 +0545

    feat: query provider

--- lib/src/core/QueryProviders/query_provider.dart
+++ lib/src/core/QueryProviders/query_provider.dart
@@ -1,101 +1,27 @@
-import 'dart:convert';
-
 import 'package:dio/dio.dart';
-import 'package:flutter/widgets.dart';
-import 'package:rxdart/rxdart.dart';
-import 'package:watamuki/src/injector.dart';
+import 'package:watamuki/src/core/QueryProviders/behaviours/query_behaviour.dart';
 
-import 'converters/converter.dart';
-import 'converters/converter_not_found.dart';
 import 'models/params.dart';
 import 'models/query_context.dart';
-import 'models/query_object.dart';
-import 'query_injectors.dart';
+import 'providers/base_query_provider.dart';
 
-class QueryProvider<T extends dynamic> {
-  final ResponseConverter converter = getItQuery.get<ResponseConverter>();
-
-  String _queryKey = "";
-  late final String _query;
-  late Params? params;
-  late QueryContext _queryContext;
-
-  final BehaviorSubject<QueryObject<T>> _data = BehaviorSubject();
-
-  Stream<QueryObject<T>> get dataStream => _data.stream;
-
-  QueryObject<T> get data => _data.value;
-
-  final Future<Response> Function({QueryContext context}) _queryFn;
-
-  dynamic Function(Map<String, dynamic>)? select;
-
-  void Function(T data)? onSuccess;
-  void Function(Exception error)? onError;
-  bool fetchOnMount;
-
-  late final Future Function() refetch;
-
-  QueryProvider(this._query,
-      this._queryFn, {
-        this.params,
-        this.fetchOnMount = true,
-        this.onSuccess,
-        this.onError,
-        this.select,
-      }) {
-    refetch = () async {
-      _queryKey = [_query, params?.toJson()].toString();
-      _queryContext = QueryContext(queryKey: [_query, params]);
-
-      final hasData = sharedPreferences.containsKey(_queryKey);
-      if (hasData) {
-        try {
-          T cacheData = converter.convert<T>(
-              jsonDecode(sharedPreferences.get(_queryKey) as String));
-          _data.add(
-              QueryObject(isLoading: false, isFetching: true, data: cacheData));
-          if (onSuccess != null) onSuccess!(cacheData);
-        } on ConverterNotFountException catch (e) {
-          debugPrint(e.message);
-        }
-      } else {
-        _data.add(QueryObject(isLoading: true, isFetching: true));
-      }
-
-      try {
-        final res = await _queryFn(context: _queryContext);
-        final data = select!(res.data) ?? res.data;
-        final parsedData = converter.convert<T>(data);
-
-        _data.add(
-            QueryObject(isLoading: false, isFetching: false, data: parsedData));
-        sharedPreferences.setString(_queryKey, jsonEncode(parsedData));
-
-        if (onSuccess != null) onSuccess!(parsedData);
-      } on Exception catch (e) {
-        if (e is ConverterNotFountException) {
-          debugPrint(e.message);
-        }
-        if (onError != null) onError!(e);
-      }
-    };
-
-    if (fetchOnMount) {
-      refetch();
-    }
-  }
-
-  void clearCache() {
-    if (_queryKey.isNotEmpty) {
-      sharedPreferences.remove(_queryKey);
-    }
-  }
-
-  void revalidateCache() {
-    if (_queryKey.isNotEmpty) {
-      sharedPreferences.remove(_queryKey);
-      refetch();
-    }
-  }
+class QueryProvider<T extends dynamic> extends BaseQueryProvider<T> {
+  QueryProvider(
+    String query,
+    Future<Response> Function({QueryContext context}) queryFn, {
+    Params? params,
+    bool fetchOnMount = true,
+    void Function(T data)? onSuccess,
+    void Function(Exception error)? onError,
+    dynamic Function(Map<String, dynamic>)? select,
+  }) : super(
+          QueryBehaviour<T>(),
+          query,
+          queryFn,
+          params: params,
+          fetchOnMount: fetchOnMount,
+          onSuccess: onSuccess,
+          onError: onError,
+          select: select,
+        );
 }
