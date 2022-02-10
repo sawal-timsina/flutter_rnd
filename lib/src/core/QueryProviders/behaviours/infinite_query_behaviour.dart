151836c3fbb8c1dde719677cd605f41432890986
Sawal Timsina <t.sawal231@gmail.com>
2022-02-10 23:58:21 +0545

    feat: infinite query on fetch, updating old data

--- lib/src/core/QueryProviders/behaviours/infinite_query_behaviour.dart
+++ lib/src/core/QueryProviders/behaviours/infinite_query_behaviour.dart
@@ -21,25 +21,55 @@ class InfiniteQueryBehaviour<T extends dynamic> extends Behaviour<List<T>> {
     return (data as List).map<T>((e) => converter.convert<T>(e)).toList();
   }
 
-  @override
-  List<T> parseData(data, previousData) {
-    final parsedData = converter.convert<T>(data);
+  List<T> revalidateData(data, previousData,
+      {bool forceRefresh = false, required String queryKey}) {
+    List<T> tdList = forceRefresh ? [data] : previousData ?? [];
+    if (!forceRefresh && data.isNotEmpty) {
+      // [1] check if new list's item are already present or not
+      bool containsNew = (tdList.isEmpty
+          ? false
+          : tdList.every((element) {
+              if (element is! List) return false;
+              return element.every((ee) {
+                return data.contains(ee);
+              });
+            }));
+      // [1]
 
-    List<T> tdList = previousData ?? [];
-    // [1] check if new list's item are already present or not
-    bool containsNew = (tdList.isEmpty
-        ? false
-        : tdList.every((element) {
-            if (element is! List) return false;
-            return element.every((ee) {
-              return parsedData.contains(ee);
-            });
-          }));
-    // [1]
+      if (!containsNew) {
+        tdList.add(data);
+      }
+    }
 
-    if (!containsNew) {
-      tdList.add(parsedData);
+    final _nextPageParams = _getNextPageParam!(tdList.last) as String;
+    if (_nextPageParams.isNotEmpty) {
+      onNextPageParams!(InfiniteQueryParams(
+          _nextPageParams.isNotEmpty, _nextPageParams, queryKey));
     }
     return tdList;
+  }
+
+  @override
+  Future<List<T>> onFetch(BehaviourContext<List<T>> context) async {
+    final queryContext = context.queryContext;
+    final queryKey = context.queryKey;
+    final res = await context.queryFn(context: queryContext);
+
+    final parsedData =
+        converter.convert<T>(context.select!(res.data) ?? res.data);
+    if (context.forceRefresh && paramsList.containsKey(queryKey)) {
+      for (final element in paramsList[queryKey]!) {
+        final res =
+            await context.queryFn(context: queryContext..pageParam = element);
+
+        final _parsedData =
+            converter.convert<T>(context.select!(res.data) ?? res.data);
+
+        parsedData.addAll(_parsedData);
+      }
+    }
+
+    return revalidateData(parsedData, context.cacheData,
+        forceRefresh: context.forceRefresh, queryKey: queryKey);
   }
 }
